6d38ffe85b91d3bd4388b579ccf073f103e4b582
v1ynlee <chikaamatori27@gmail.com>
2026-06-10 03:08:48 +0700

    feat: update studio article routes

---
+++ src/app/studio/articles/loading.tsx
@@ -0,0 +1,5 @@
+import { ArticleDashboardSkeleton } from '@/components/studio/articles/ArticleDashboardSkeleton';
+
+export default function StudioArticlesLoading() {
+  return <ArticleDashboardSkeleton />;
+}
